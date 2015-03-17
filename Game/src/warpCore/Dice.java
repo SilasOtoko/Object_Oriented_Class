0fbe8ca5d52d3b1460b41174ec6b0f86f8067f36
Josiah Schaefer <penname.silas@gmail.com>
2015-03-17 14:06:19 -0400

    3/17/15 First Test Dice Program

--- Game/src/warpCore/Dice.java
+++ Game/src/warpCore/Dice.java
@@ -15,4 +15,32 @@ public class Dice {
 		{
 			setSides(startSides);
 		}
+		
+		//roll method
+		/* this method takes no parameters
+		 * and returns a single random integer in the range of 1 to size
+		 */
+		public int roll()
+		{
+			int n = (int)(Math.random()* getSides() + 1);
+			return n;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return getSides() + " sided dice";
+		}
+		
+		public boolean equals(Dice other)
+		{
+			if(other.sides == getSides())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 }
--- Game/src/warpCore/WarpCore.java
+++ Game/src/warpCore/WarpCore.java
@@ -3,8 +3,13 @@ package warpCore;
 public class WarpCore {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Input input = new Input();
+		Dice d = new Dice();
+		Dice coin = new Dice(2);
+		System.out.println(d.toString());
+		System.out.println(coin.toString());
+		int newDie = input.getInt("enter sides: ");
+		Dice nd = new Dice(newDie);
+		System.out.println(nd.toString());
 	}
-
 }
